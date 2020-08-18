bf7e2019fdb5f9edb47757b4e053144f29d18964
Josh <39458297+jgagan95@users.noreply.github.com>
2020-08-17 21:12:49 -0500

    Add files via upload

---
+++ pokemon.py
@@ -0,0 +1,23 @@
+from random import randint
+#welcome message
+print("WELCOME TO BOOTLEG POKEMON")
+
+#select a pokmemon and save it to your party
+pokemon_party = []
+print("\n\n\n\n\nHere are the three pokemon to select: Bulbasaur, Charmander, and Squirtle.")
+starter_pokemon = input("Select a pokemon from that list: ")
+
+while pokemon_party:
+    if starter_pokemon
+
+'''
+#create random pokemon to encounter
+encounter_pokemon = ['Pidgey', 'Ratata']
+'''
+
+#simulate the starting area of pokemon red
+
+#welcome message
+#select a pokemon
+#determine rival pokemon (opposite of what you select)
+#small battle
