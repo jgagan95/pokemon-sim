53c68da098c9e9e0fd4c389752714dbb62eb39f5
Josh <39458297+jgagan95@users.noreply.github.com>
2020-08-18 19:27:52 -0500

    Update pokemon.py

--- pokemon.py
+++ pokemon.py
@@ -5,19 +5,15 @@ print("WELCOME TO BOOTLEG POKEMON")
 #select a pokmemon and save it to your party
 pokemon_party = []
 print("\n\n\n\n\nHere are the three pokemon to select: Bulbasaur, Charmander, and Squirtle.")
-starter_pokemon = input("Select a pokemon from that list: ")
 
-while pokemon_party:
-    if starter_pokemon
+while True:
+    starter_pokemon = input("Select a pokemon from that list: ").lower()
+    if starter_pokemon == "bulbasaur" or starter_pokemon == "charmander" or starter_pokemon == "squirtle":
+        print(f"All right! You've chosen {starter_pokemon.capitalize()}!")
+        pokemon_party.append(starter_pokemon)
+        break
+    else:
+        print("\nPlease select from the choices given")
 
-'''
-#create random pokemon to encounter
-encounter_pokemon = ['Pidgey', 'Ratata']
-'''
-
-#simulate the starting area of pokemon red
-
-#welcome message
-#select a pokemon
 #determine rival pokemon (opposite of what you select)
 #small battle
