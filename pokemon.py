bcecf4a07b3baab81892b0d633832898947b7467
Josh <39458297+jgagan95@users.noreply.github.com>
2020-08-28 23:24:29 -0500

    Update pokemon.py

--- pokemon.py
+++ pokemon.py
@@ -16,17 +16,31 @@ while True:
         print("\nPlease select from the choices given")
 
 #determine rival pokemon (opposite of what you select)
+
+charmander_moves = ['Scratch', 'Growl']
+
+squirtle_moves = ['Tackle', 'Tail Whip']
+
+bulbasaur_moves = ['Tackle', 'Growl']
+
 rival_party = []
 if starter_pokemon == "bulbasaur":
     rival_party.append("Charmander")
+    rival_party.append(charmander_moves)
+    pokemon_party.append(bulbasaur_moves)
 elif starter_pokemon == "squirtle":
     rival_party.append("Bulbasaur")
+    rival_party.append(bulbasaur_moves)
+    pokemon_party.append(squirtle_moves)
 else:
     rival_party.append("Squirtle")
+    rival_party.append(squirtle_moves)
+    pokemon_party.append(charmander_moves)
 
 print(f"\n\n\n\n\nMeanwhile, your opponent has chosen {rival_party[0]}!")
 
 #small battle
+
 #assign movesets and stats to pokemon based off of selection
 pokemon_stats = {
     'HP': 19,
@@ -35,8 +49,13 @@ pokemon_stats = {
     'Speed': randint(10, 12)
 }
 
-charmander_moves = ['Scratch', 'Growl']
+your_pokemon = {}.fromkeys([starter_pokemon.capitalize()], pokemon_party[1])
+your_pokemon.update(pokemon_stats)
+print(your_pokemon)
 
-squirtle_moves = ['Tackle', 'Tail Whip']
+rival_pokemon = {}.fromkeys([rival_party[0]], rival_party[1])
+rival_pokemon.update(pokemon_stats)
+print(rival_pokemon)
 
-bulbasaur_moves = ['Tackle', 'Growl']
+# need to fix random, so I may use choice in random module
+#start on battle portion
