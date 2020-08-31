ab69c2e346631e5c74643174cf5b48c4198bf6ec
Josh <39458297+jgagan95@users.noreply.github.com>
2020-08-30 21:16:32 -0500

    Update pokemon.py

--- pokemon.py
+++ pokemon.py
@@ -1,9 +1,28 @@
 from random import randint
+pokemon_party = []
+rival_party = []
+
+pokemon_stats = {
+    'HP': 19,
+    'Attack': randint(10, 12),
+    'Defense': randint(10, 12),
+    'Speed': randint(10, 12)
+}
+
+#had to create another dictionary similar to pokemon stats to get randomness on stats
+rival_stats = {
+    'HP': 19,
+    'Attack': randint(10, 12),
+    'Defense': randint(10, 12),
+    'Speed': randint(10, 12)
+}
+
+charmander_moves = ['Scratch', 'Growl']
+squirtle_moves = ['Tackle', 'Tail Whip']
+bulbasaur_moves = ['Tackle', 'Growl']
+
 #welcome message
 print("WELCOME TO BOOTLEG POKEMON")
-
-#select a pokmemon and save it to your party
-pokemon_party = []
 print("\n\n\n\n\nHere are the three pokemon to select: Bulbasaur, Charmander, and Squirtle.")
 
 while True:
@@ -15,48 +34,28 @@ while True:
     else:
         print("\nPlease select from the choices given")
 
-#determine rival pokemon (opposite of what you select)
-
-charmander_moves = ['Scratch', 'Growl']
-
-squirtle_moves = ['Tackle', 'Tail Whip']
-
-bulbasaur_moves = ['Tackle', 'Growl']
-
-rival_party = []
-if starter_pokemon == "bulbasaur":
+def rival_starter():
+  if starter_pokemon == "bulbasaur":
     rival_party.append("Charmander")
     rival_party.append(charmander_moves)
     pokemon_party.append(bulbasaur_moves)
-elif starter_pokemon == "squirtle":
+  elif starter_pokemon == "squirtle":
     rival_party.append("Bulbasaur")
     rival_party.append(bulbasaur_moves)
     pokemon_party.append(squirtle_moves)
-else:
+  else:
     rival_party.append("Squirtle")
     rival_party.append(squirtle_moves)
     pokemon_party.append(charmander_moves)
 
-print(f"\n\n\n\n\nMeanwhile, your opponent has chosen {rival_party[0]}!")
+  return rival_party[0]
 
-#small battle
-
-#assign movesets and stats to pokemon based off of selection
-pokemon_stats = {
-    'HP': 19,
-    'Attack': randint(10, 12),
-    'Defense': randint(10, 12),
-    'Speed': randint(10, 12)
+rival = {
+  'Starter': rival_starter(),
+  'Moveset': rival_party[1]
 }
 
-your_pokemon = {}.fromkeys([starter_pokemon.capitalize()], pokemon_party[1])
-your_pokemon.update(pokemon_stats)
-print(your_pokemon)
-
-rival_pokemon = {}.fromkeys([rival_party[0]], rival_party[1])
-rival_pokemon.update(pokemon_stats)
-print(rival_pokemon)
-
-# need to fix random, so I may use choice in random module
-#start on battle portion
-# figure out ways to imrove randomness from pokemon_stats
+your_party = {
+  'Starter': pokemon_party[0].capitalize(),
+  'Moveset': pokemon_party[1]
+}
