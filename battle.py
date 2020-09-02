92db2f4f4ad6a6ba688c384c844be16010496f0f
Josh <39458297+jgagan95@users.noreply.github.com>
2020-09-01 22:09:40 -0500

    Update battle.py

--- battle.py
+++ battle.py
@@ -1,9 +1,10 @@
 from pokemon import *
-from random import choice
-
+from random import choice, randint
 
 print("\n\n\n\n\nRival: Let's check out our pokemon\nCome on! I'll take you on")
 print(f"Rival would like to battle. They sent out {rival_party[0]}")
+
+match_over = False
 
 #need to determine which side goes first, but not a coinflip
 #calculating stat reducing with abilities
