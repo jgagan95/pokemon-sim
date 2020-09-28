d442b5816687141f93ca9c1ded00864aed7de5b5
Josh <39458297+jgagan95@users.noreply.github.com>
2020-09-27 19:30:28 -0500

    Update battle.py

--- battle.py
+++ battle.py
@@ -1,12 +1,25 @@
 from pokemon import *
-game_over = False
 
 def game():
-  print(f'\n\nWhat attack would you like to use? {your_party["Moveset"][0]} or {your_party["Moveset"][1]}')
+  while True:
+    if rival["HP"] == 19 and your_party["HP"] == 19:
+      my_attack()
+      attack = rival_attack()
+      print(f'\n\nYour rival used: {attack}')
+      print(f'Your HP is: {your_party["HP"]}, Attack is: {your_party["Attack"]}, Defense is: {your_party["Defense"]}')
+      print(f'Your Rival\'s HP is: {rival["HP"]}, Attack is: {rival["Attack"]}, Defense is: {rival["Defense"]}')
+    else:
+      if rival["HP"] <= 0:
+        print("YOU WIN")
+        break
+      elif your_party["HP"] <= 0:
+        print("YOU LOSE")
+        break
+      else:
+        my_attack()
+        attack = rival_attack()
+        print(f'\n\nYour rival used: {attack}')
+        print(f'Your HP is: {your_party["HP"]}, Attack is: {your_party["Attack"]}, Defense is: {your_party["Defense"]}')
+        print(f'Your Rival\'s HP is: {rival["HP"]}, Attack is: {rival["Attack"]}, Defense is: {rival["Defense"]}')
 
 game()
-
-
-
-#random_attack = choice(attack_list)()
-#need to get code onto pokemon.py then run the battle on this file
